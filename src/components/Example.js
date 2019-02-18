01f3fa25c2aa7f504b8aa3d71d070f84d45a439e
Charles <carlos.aaron.villanueva@gmail.com>
2019-02-18 17:24:06 +0100

    Play with Suspense Lazy

---
+++ src/components/Example.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default  props => {
+
+	return (
+		<img alt="example" src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Salzburg_from_Gaisberg_big_version.jpg" />
+	);
+};
